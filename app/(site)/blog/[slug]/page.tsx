294732f0681657aa2d08246657d1b5b81f4b92ac
Caleb <48822049+cabennetts@users.noreply.github.com>
2023-09-05 16:06:20 -0500

    Fix mdx styling error

--- app/(site)/blog/[slug]/page.tsx
+++ app/(site)/blog/[slug]/page.tsx
@@ -51,37 +51,39 @@ export default async function Post({ params: { slug } }: Props) {
     ))
 
     return (
-        <main className="content__main">
-            <section className="mb-4">
-                <h1 className="text-5xl font-bold row-span-2 p-6 cursor-pointer tracking-wider md:text-5xl md:col-span-2"> {meta.title} </h1>
-                <p className="px-6 text-sm"> {pubDate} </p>
-            </section>
-            
-            <Separator color="bg-white" className="my-6"/>
+        <>
+            <main className="content__main">
+                <section className="mb-4">
+                    <h1 className="text-5xl font-bold row-span-2 p-6 cursor-pointer tracking-wider md:text-5xl md:col-span-2"> {meta.title} </h1>
+                    <p className="px-6 text-sm"> {pubDate} </p>
+                </section>
+                
+                <Separator color="bg-white" className="my-6"/>
 
-            <article>
-                {content}
-            </article>
-            
-            <Separator color="bg-white" className="my-6"/>
+                <article>
+                    {content}
+                </article>
+                
+                <Separator color="bg-white" className="my-6"/>
 
-            <section className="p-6 bg-[#111111] rounded-xl">
-                <h3 className="text-2xl ">Related:</h3>
-                <div className="flex flex-row gap-4">
-                    {tags}
-                </div>
-            </section>
-            <section className='flex ml-6 mt-6 flex-col md:flex-row'>
-                <div className='md:pr-6 flex items-center'>
-                    <Link href="/blog">
-                        <p className='text-lg w-fit rounded-xl cursor-pointer text-center hover:text-blue-500 md:text-2xl'>
-                            Other Posts
-                        </p>
-                    </Link>
-                </div>
-                <ShareBtn url={`https://cabennetts.dev/blog/${slug}`}/>
-            </section>
+                <section className="p-6 bg-[#111111] rounded-xl">
+                    <h3 className="text-2xl ">Related:</h3>
+                    <div className="flex flex-row gap-4">
+                        {tags}
+                    </div>
+                </section>
+                <section className='flex ml-6 mt-6 flex-col md:flex-row'>
+                    <div className='md:pr-6 flex items-center'>
+                        <Link href="/blog">
+                            <p className='text-lg w-fit rounded-xl cursor-pointer text-center hover:text-blue-500 md:text-2xl'>
+                                Other Posts
+                            </p>
+                        </Link>
+                    </div>
+                    <ShareBtn url={`https://cabennetts.com/blog/${slug}`}/>
+                </section>
 
-        </main>
+            </main>
+        </>
     )
 }
